90e95c231308b329b6d13ed188f1d0c65c7b3116
Vanshika1704 <vanshikawadhwa1704@gmail.com>
2024-06-15 17:23:25 +0530

    Added redirect if someone searches /home to redirect to /

--- src/router/index.js
+++ src/router/index.js
@@ -19,6 +19,10 @@ const router =createRouter({
             component: HomeView
         },
         {
+            path: "/home",
+            redirect: "/" 
+        },
+        {
             path: "/about",
             name: "about",
             component: AboutView
