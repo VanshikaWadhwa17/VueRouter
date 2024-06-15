68023643d778cc5ea377692ca8a3df4f6a2b0a1b
Vanshika1704 <vanshikawadhwa1704@gmail.com>
2024-06-15 16:27:38 +0530

    Nested routing added

--- src/views/CarView.vue
+++ src/views/CarView.vue
@@ -1,5 +1,5 @@
 <script setup>
-import {useRoute} from "vue-router"
+import {useRoute , RouterView} from "vue-router"
 import cars from "../data/cars.json"
 
 const route =useRoute()
@@ -10,12 +10,10 @@ const car= cars.find(c=>c.id===parseInt(route.params.id))
 </script>
 <template>
     <div>
-        <h1>
-            CarView
-           
-        </h1>
+       <h1>Car view</h1>
         <p>{{ car.name }}</p>
         <p>{{ car.year }}</p>
         <p>{{ car.price }}</p>
+        <RouterView/>
     </div>
 </template>
