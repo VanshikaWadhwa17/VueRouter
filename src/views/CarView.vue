3af03284d002f631eb41e70bac0c3d6c52de38e0
Vanshika1704 <vanshikawadhwa1704@gmail.com>
2024-06-15 16:36:10 +0530

    rendering roites using navigation buttons

--- src/views/CarView.vue
+++ src/views/CarView.vue
@@ -1,11 +1,16 @@
 <script setup>
-import {useRoute , RouterView} from "vue-router"
+import {useRoute , useRouter, RouterView} from "vue-router"
 import cars from "../data/cars.json"
 
 const route =useRoute()
 //console.log(route.params)
+// useRoute allows us to extract information about the current path that we are in
+// useRouter allows us to make adjustments/modifiactions in our path
+const router= useRouter()
+const carId=route.params.id
 
-const car= cars.find(c=>c.id===parseInt(route.params.id))
+
+const car= cars.find(c=>c.id===parseInt(carId))
 
 </script>
 <template>
@@ -14,6 +19,7 @@ const car= cars.find(c=>c.id===parseInt(route.params.id))
         <p>{{ car.name }}</p>
         <p>{{ car.year }}</p>
         <p>{{ car.price }}</p>
+        <button @click="router.push(`/cars/${carId}/contact`)">Contact Dealer</button>
         <RouterView/>
     </div>
 </template>
